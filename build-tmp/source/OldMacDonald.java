4d496a14f06090f6012a3ccbeed3b9084707f69c
unknown <krleung@697-334-Shelob.sf.k12.internal>
2014-12-01 12:04:01 -0800

    finally finished it

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 //your variable declarations here
-void setup()
+public void setup()
 {
     Farm f = new Farm();
     f.animalSounds();
@@ -88,3 +104,12 @@ class Chick implements Animal
 	public String getType(){return myType;} 
 }
 //declare classes and the interface below
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
